d033a44a0b79fb6e22cf58155014a18b71d68d27
Kisliankou Viachaslau <kislenkov.vs@gmail.com>
2023-04-29 19:20:26 +0300

    refactor: apply class Board

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-const textArea = document.createElement('textarea');
-textArea.className = 'text-area';
-document.body.prepend(textArea);
-/* let engBoard = [];
+
+/*let engBoard = [];
 document.addEventListener("keydown", (event) => {
     let k = {};
     k.key = event.key;
@@ -10,6 +8,7 @@ document.addEventListener("keydown", (event) => {
     if (k.code === "MetaLeft") { k.key ="Win"};
     if (k.code === "ControlLeft" || k.code === "ControlRight") { k.key ="Ctrl"};
     engBoard.push(k);
+    if (k.key === 'Shift') {engBoard.pop()}
     console.log(engBoard);
 }) */
 const englishBoardLowerCase = [
@@ -93,46 +92,186 @@ const englishBoardLowerCase = [
   ],
 ];
 
-const boardWrapper = document.createElement('div');
-boardWrapper.className = 'board-wrapper';
-textArea.after(boardWrapper);
+const englishBoardShiftCase = [
+  [
+    { key: '~', code: 'Backquote' },
+    { key: '!', code: 'Digit1' },
+    { key: '@', code: 'Digit2' },
+    { key: '#', code: 'Digit3' },
+    { key: '$', code: 'Digit4' },
+    { key: '%', code: 'Digit5' },
+    { key: '^', code: 'Digit6' },
+    { key: '&', code: 'Digit7' },
+    { key: '*', code: 'Digit8' },
+    { key: '(', code: 'Digit9' },
+    { key: ')', code: 'Digit0' },
+    { key: '_', code: 'Minus' },
+    { key: '+', code: 'Equal' },
+    { key: 'Backspace', code: 'Backspace' },
+  ],
+  [
+    { key: 'Tab', code: 'Tab' },
+    { key: 'Q', code: 'KeyQ' },
+    { key: 'W', code: 'KeyW' },
+    { key: 'E', code: 'KeyE' },
+    { key: 'R', code: 'KeyR' },
+    { key: 'T', code: 'KeyT' },
+    { key: 'Y', code: 'KeyY' },
+    { key: 'U', code: 'KeyU' },
+    { key: 'I', code: 'KeyI' },
+    { key: 'O', code: 'KeyO' },
+    { key: 'P', code: 'KeyP' },
+    { key: '{', code: 'BracketLeft' },
+    { key: '}', code: 'BracketRight' },
+    { key: '|', code: 'Backslash' },
+    { key: 'Delete', code: 'Delete' },
+  ],
+  [
+    { key: 'CapsLock', code: 'CapsLock' },
+    { key: 'A', code: 'KeyA' },
+    { key: 'S', code: 'KeyS' },
+    { key: 'D', code: 'KeyD' },
+    { key: 'F', code: 'KeyF' },
+    { key: 'G', code: 'KeyG' },
+    { key: 'H', code: 'KeyH' },
+    { key: 'J', code: 'KeyJ' },
+    { key: 'K', code: 'KeyK' },
+    { key: 'L', code: 'KeyL' },
+    { key: ':', code: 'Semicolon' },
+    { key: '"', code: 'Quote' },
+    { key: 'Enter', code: 'Enter' },
+  ],
+  [
+    { key: 'Shift', code: 'ShiftLeft' },
+    { key: 'Z', code: 'KeyZ' },
+    { key: 'X', code: 'KeyX' },
+    { key: 'C', code: 'KeyC' },
+    { key: 'V', code: 'KeyV' },
+    { key: 'B', code: 'KeyB' },
+    { key: 'N', code: 'KeyN' },
+    { key: 'M', code: 'KeyM' },
+    { key: '<', code: 'Comma' },
+    { key: '>', code: 'Period' },
+    { key: '?', code: 'Slash' },
+    { key: '&uarr;', code: 'ArrowUp' },
+    { key: 'Shift', code: 'ShiftRight' },
+  ],
+  [
+    { key: 'Ctrl', code: 'ControlLeft' },
+    { key: 'Win', code: 'MetaLeft' },
+    { key: 'Alt', code: 'AltLeft' },
+    { key: ' ', code: 'Space' },
+    { key: 'Alt', code: 'AltRight' },
+    { key: '&larr;', code: 'ArrowLeft' },
+    { key: '&darr;', code: 'ArrowDown' },
+    { key: '&rarr;', code: 'ArrowRight' },
+    { key: 'Ctrl', code: 'ControlRight' },
+  ],
+]
 
-function printSymbol() {
-  textArea.focus();
-  textArea.value += this.innerHTML;
-}
 
-const buttons =[];
-for (let i = 0; i < 5; i++) {
-  const boardStringWrapper = [];
-  boardStringWrapper[i] = document.createElement('div');
-  boardStringWrapper[i].className = 'board-wrapper__string';
-  boardWrapper.append(boardStringWrapper[i]);
-  for (let j = 0; j < englishBoardLowerCase[i].length; j++) {
-    const button = document.createElement('button');
-    button.className = 'button';
-    if (englishBoardLowerCase[i][j].key === ' ' || englishBoardLowerCase[i][j].key === 'Backspace' || englishBoardLowerCase[i][j].key === 'Tab' || englishBoardLowerCase[i][j].key === 'Delete' || englishBoardLowerCase[i][j].key === 'CapsLock' || englishBoardLowerCase[i][j].key === 'Enter' || englishBoardLowerCase[i][j].key === 'Shift' || englishBoardLowerCase[i][j].key === 'Shift') { button.style.flexGrow = 1; }
-    button.innerHTML = englishBoardLowerCase[i][j].key;
-    button.code = englishBoardLowerCase[i][j].code;
-    boardStringWrapper[i].append(button);
-    buttons.push(button);
-    button.addEventListener('click', printSymbol);
+
+class Board {
+  textArea= null;
+  createTextArea() {
+    const textArea = document.createElement('textarea');
+    textArea.className = 'text-area';
+    document.body.prepend(textArea);
+    return textArea;
   }
-}
 
+  createBoardLines(base) {
+    const boardWrapper = document.createElement('div');
+    boardWrapper.className = 'board-wrapper';
+    this.textArea.after(boardWrapper);
+    const boardStringWrapper = [];
 
-function highlightKey(event) {
- textArea.focus();
- for (let elem of buttons) {
-  if (event.code === elem.code) {
-    elem.style.backgroundColor = 'orange';
-    elem.style.borderRadius = '20px';
-    setTimeout(() => {
-      elem.style.backgroundColor = '';
-    elem.style.borderRadius = '';
-    }, 300);
+    for (let i = 0; i < base.length; i++) {
+      boardStringWrapper[i] = document.createElement('div');
+      boardStringWrapper[i].className = 'board-wrapper__string';
+      boardWrapper.append(boardStringWrapper[i]);
+    }
+
+    return boardStringWrapper;
   }
- }
+
+  createBoardButtons(base) {
+    const buttons = [];
+    for (let i = 0; i < base.length; i++) {
+
+      for (let j = 0; j < base[i].length; j++) {
+        const button = document.createElement('button');
+        button.className = 'button';
+        if (base[i][j].key === ' ' || base[i][j].key === 'Backspace' || base[i][j].key === 'Tab' || base[i][j].key === 'Delete' || base[i][j].key === 'CapsLock' || base[i][j].key === 'Enter' || base[i][j].key === 'Shift' || base[i][j].key === 'Shift') { button.style.flexGrow = 1; }
+        button.innerHTML = base[i][j].key;
+        button.code = base[i][j].code;
+        this.boardStringWrapper[i].append(button);
+        buttons.push(button);
+        button.addEventListener('click', (event) => {this.printSymbol.apply(this, event)});
+      }
+
+    }
+    window.addEventListener('keydown', (event) => {this.highlightKey.apply(this, event)});
+    window.addEventListener('keyup', (event) => {this.abortHighlightKey.apply(this, event)});
+    return buttons;
+  }
+
+  printSymbol() {
+    this.textArea.focus();
+    this.textArea.value += event.target.innerHTML;
+  }
+
+  removeBoard() {
+    this.boardStringWrapper[0].parentNode.remove();
+
+    for (let elem of this.boardStringWrapper) {
+      elem.remove();
+    }
+    
+  }
+
+  highlightKey() {
+    this.textArea.focus();
+
+    for (let elem of this.buttons) {
+      if (event.code === elem.code) {
+        elem.style.backgroundColor = 'orange';
+        elem.style.borderRadius = '20px';
+        /*if (event.key === 'Shift') {
+          this.removeBoard();
+          this.boardStringWrapper = this.createBoardLines(englishBoardShiftCase);
+          this.buttons = this.createBoardButtons(englishBoardShiftCase);
+          elem.style.backgroundColor = 'orange';
+          elem.style.borderRadius = '20px';
+        }*/
+      }
+    }
+
+  }
+
+  abortHighlightKey() {
+    this.textArea.focus();
+
+    for (let elem of this.buttons) {
+      if (event.code === elem.code) {
+        elem.style.backgroundColor = '';
+        elem.style.borderRadius = '';
+        /*if (event.key === 'Shift') {
+          this.removeBoard();
+          this.boardStringWrapper = this.createBoardLines(englishBoardLowerCase);
+          this.buttons = this.createBoardButtons(englishBoardLowerCase);
+        }*/
+      }
+    }
+    
+
+  }
+
 }
 
-window.addEventListener('keydown', highlightKey);
+const board = new Board();
+board.textArea = board.createTextArea();
+board.boardStringWrapper = board.createBoardLines(englishBoardLowerCase);
+board.buttons = board.createBoardButtons(englishBoardLowerCase);
+
+
