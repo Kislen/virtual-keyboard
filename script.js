7ecaf454090d3528450dbcea594c9a1b6b5f6aee
Kisliankou Viachaslau <kislenkov.vs@gmail.com>
2023-04-30 12:07:20 +0300

    fix: edit shift-method

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const englishBoardLowerCase = [
   ],
 ];
 
-const englishBoardShiftCase = [
+const englishBoardShiftUpperCase = [
   [
     { key: '~', code: 'Backquote' },
     { key: '!', code: 'Digit1' },
@@ -157,6 +157,84 @@ const englishBoardShiftCase = [
     { key: 'Ctrl', code: 'ControlRight' },
   ],
 ]
+
+const englishBoardShiftLowerCase = [
+  [
+    { key: '~', code: 'Backquote' },
+    { key: '!', code: 'Digit1' },
+    { key: '@', code: 'Digit2' },
+    { key: '#', code: 'Digit3' },
+    { key: '$', code: 'Digit4' },
+    { key: '%', code: 'Digit5' },
+    { key: '^', code: 'Digit6' },
+    { key: '&', code: 'Digit7' },
+    { key: '*', code: 'Digit8' },
+    { key: '(', code: 'Digit9' },
+    { key: ')', code: 'Digit0' },
+    { key: '_', code: 'Minus' },
+    { key: '+', code: 'Equal' },
+    { key: 'Backspace', code: 'Backspace' },
+  ],
+  [
+    { key: 'Tab', code: 'Tab' },
+    { key: 'q', code: 'KeyQ' },
+    { key: 'w', code: 'KeyW' },
+    { key: 'e', code: 'KeyE' },
+    { key: 'r', code: 'KeyR' },
+    { key: 't', code: 'KeyT' },
+    { key: 'y', code: 'KeyY' },
+    { key: 'u', code: 'KeyU' },
+    { key: 'i', code: 'KeyI' },
+    { key: 'o', code: 'KeyO' },
+    { key: 'p', code: 'KeyP' },
+    { key: '{', code: 'BracketLeft' },
+    { key: '}', code: 'BracketRight' },
+    { key: '|', code: 'Backslash' },
+    { key: 'Delete', code: 'Delete' },
+  ],
+  [
+    { key: 'CapsLock', code: 'CapsLock' },
+    { key: 'a', code: 'KeyA' },
+    { key: 's', code: 'KeyS' },
+    { key: 'd', code: 'KeyD' },
+    { key: 'f', code: 'KeyF' },
+    { key: 'g', code: 'KeyG' },
+    { key: 'h', code: 'KeyH' },
+    { key: 'j', code: 'KeyJ' },
+    { key: 'k', code: 'KeyK' },
+    { key: 'l', code: 'KeyL' },
+    { key: ':', code: 'Semicolon' },
+    { key: '"', code: 'Quote' },
+    { key: 'Enter', code: 'Enter' },
+  ],
+  [
+    { key: 'Shift', code: 'ShiftLeft' },
+    { key: 'z', code: 'KeyZ' },
+    { key: 'x', code: 'KeyX' },
+    { key: 'c', code: 'KeyC' },
+    { key: 'v', code: 'KeyV' },
+    { key: 'b', code: 'KeyB' },
+    { key: 'n', code: 'KeyN' },
+    { key: 'm', code: 'KeyM' },
+    { key: '<', code: 'Comma' },
+    { key: '>', code: 'Period' },
+    { key: '?', code: 'Slash' },
+    { key: '&uarr;', code: 'ArrowUp' },
+    { key: 'Shift', code: 'ShiftRight' },
+  ],
+  [
+    { key: 'Ctrl', code: 'ControlLeft' },
+    { key: 'Win', code: 'MetaLeft' },
+    { key: 'Alt', code: 'AltLeft' },
+    { key: ' ', code: 'Space' },
+    { key: 'Alt', code: 'AltRight' },
+    { key: '&larr;', code: 'ArrowLeft' },
+    { key: '&darr;', code: 'ArrowDown' },
+    { key: '&rarr;', code: 'ArrowRight' },
+    { key: 'Ctrl', code: 'ControlRight' },
+  ],
+]
+
 const englishBoardUpperCase = [
   [
     { key: '`', code: 'Backquote' },
@@ -234,6 +312,7 @@ const englishBoardUpperCase = [
     { key: 'Ctrl', code: 'ControlRight' },
   ],
 ]
+
 
 const russianBoardLowerCase = [
   [
@@ -316,7 +395,7 @@ const russianBoardLowerCase = [
   ],
 ];
 
-const russianBoardShiftCase = [
+const russianBoardShiftUpperCase = [
   [
     { key: 'Ё', code: 'Backquote' },
     { key: '!', code: 'Digit1' },
@@ -379,6 +458,87 @@ const russianBoardShiftCase = [
     { key: 'Ь', code: 'KeyM' },
     { key: 'Б', code: 'Comma' },
     { key: 'Ю', code: 'Period' },
+    { key: ',', code: 'Slash' },
+    { key: '&uarr;', code: 'ArrowUp' },
+    { key: 'Shift', code: 'ShiftRight' },
+  ],
+
+  [
+    { key: 'Ctrl', code: 'ControlLeft' },
+    { key: 'Win', code: 'MetaLeft' },
+    { key: 'Alt', code: 'AltLeft' },
+    { key: ' ', code: 'Space' },
+    { key: 'Alt', code: 'AltRight' },
+    { key: '&larr;', code: 'ArrowLeft' },
+    { key: '&darr;', code: 'ArrowDown' },
+    { key: '&rarr;', code: 'ArrowRight' },
+    { key: 'Ctrl', code: 'ControlRight' },
+  ],
+];
+
+const russianBoardShiftLowerCase = [
+  [
+    { key: 'ё', code: 'Backquote' },
+    { key: '!', code: 'Digit1' },
+    { key: '"', code: 'Digit2' },
+    { key: '№', code: 'Digit3' },
+    { key: ';', code: 'Digit4' },
+    { key: '%', code: 'Digit5' },
+    { key: ':', code: 'Digit6' },
+    { key: '?', code: 'Digit7' },
+    { key: '*', code: 'Digit8' },
+    { key: '(', code: 'Digit9' },
+    { key: ')', code: 'Digit0' },
+    { key: '_', code: 'Minus' },
+    { key: '+', code: 'Equal' },
+    { key: 'Backspace', code: 'Backspace' },
+  ],
+
+  [
+    { key: 'Tab', code: 'Tab' },
+    { key: 'й', code: 'KeyQ' },
+    { key: 'ц', code: 'KeyW' },
+    { key: 'у', code: 'KeyE' },
+    { key: 'к', code: 'KeyR' },
+    { key: 'е', code: 'KeyT' },
+    { key: 'н', code: 'KeyY' },
+    { key: 'г', code: 'KeyU' },
+    { key: 'ш', code: 'KeyI' },
+    { key: 'щ', code: 'KeyO' },
+    { key: 'з', code: 'KeyP' },
+    { key: 'х', code: 'BracketLeft' },
+    { key: 'ъ', code: 'BracketRight' },
+    { key: '/', code: 'Backslash' },
+    { key: 'Delete', code: 'Delete' },
+  ],
+
+  [
+    { key: 'CapsLock', code: 'CapsLock' },
+    { key: 'ф', code: 'KeyA' },
+    { key: 'ы', code: 'KeyS' },
+    { key: 'в', code: 'KeyD' },
+    { key: 'а', code: 'KeyF' },
+    { key: 'п', code: 'KeyG' },
+    { key: 'р', code: 'KeyH' },
+    { key: 'о', code: 'KeyJ' },
+    { key: 'л', code: 'KeyK' },
+    { key: 'д', code: 'KeyL' },
+    { key: 'ж', code: 'Semicolon' },
+    { key: 'э', code: 'Quote' },
+    { key: 'Enter', code: 'Enter' },
+  ],
+
+  [
+    { key: 'Shift', code: 'ShiftLeft' },
+    { key: 'я', code: 'KeyZ' },
+    { key: 'ч', code: 'KeyX' },
+    { key: 'с', code: 'KeyC' },
+    { key: 'м', code: 'KeyV' },
+    { key: 'и', code: 'KeyB' },
+    { key: 'т', code: 'KeyN' },
+    { key: 'ь', code: 'KeyM' },
+    { key: 'б', code: 'Comma' },
+    { key: 'ю', code: 'Period' },
     { key: ',', code: 'Slash' },
     { key: '&uarr;', code: 'ArrowUp' },
     { key: 'Shift', code: 'ShiftRight' },
@@ -520,6 +680,7 @@ class Board {
         buttons.push(button);
         button.addEventListener('click', (event) => {this.printSymbol.apply(this, event)});
         button.addEventListener('click', (event) => {this.changeBoardCase.apply(this, event)});
+        button.addEventListener('click', (event) => {this.applyShiftOnClick.apply(this, event)});
       }
 
     }
@@ -532,11 +693,16 @@ class Board {
     window.addEventListener('keyup', (event) => {this.abortHighlightKey.apply(this, event)});
     window.addEventListener('keydown', (event) => {this.changeBoard.apply(this, event)});
     window.addEventListener('keydown', (event) => {this.changeBoardCase.apply(this, event)});
+    window.addEventListener('keydown', (event) => {this.applyShift.apply(this, event)});
+    window.addEventListener('keyup', (event) => {this.ammendShift.apply(this, event)});
   }
 
   printSymbol() {
+    const arr = ['Backspace', 'Tab', 'Delete', 'CapsLock', 'Enter', 'Shift', 'Ctrl', 'Win', 'Alt']
+    if (!arr.includes(event.target.innerHTML)) {
     this.textArea.focus();
     this.textArea.value += event.target.innerHTML;
+    }
   }
 
   removeBoard() {
@@ -586,22 +752,22 @@ class Board {
   changeBoard() {
     if(event.code === 'AltLeft' && event.ctrlKey ) {
 
-      switch (sessionStorage.getItem('lang-mode')) {
+      switch (localStorage.getItem('lang-mode')) {
         case 'rus-lower':
           this.changeButtonsNotes(englishBoardLowerCase);
-          sessionStorage.setItem('lang-mode', 'eng-lower');
+          localStorage.setItem('lang-mode', 'eng-lower');
           break;
         case 'rus-upper':
           this.changeButtonsNotes(englishBoardUpperCase);
-          sessionStorage.setItem('lang-mode', 'eng-upper');
+          localStorage.setItem('lang-mode', 'eng-upper');
           break;
         case 'eng-lower':
           this.changeButtonsNotes(russianBoardLowerCase);
-          sessionStorage.setItem('lang-mode', 'rus-lower');
+          localStorage.setItem('lang-mode', 'rus-lower');
           break;
         case 'eng-upper':
           this.changeButtonsNotes(russianBoardUpperCase);
-          sessionStorage.setItem('lang-mode', 'rus-upper');
+          localStorage.setItem('lang-mode', 'rus-upper');
           break;
       
         default:
@@ -613,23 +779,22 @@ class Board {
 
   changeBoardCase() {
     if(event.code === 'CapsLock' || event.target.innerHTML === 'CapsLock' ) {
-
-      switch (sessionStorage.getItem('lang-mode')) {
+      switch (localStorage.getItem('lang-mode')) {
         case 'rus-lower':
           this.changeButtonsNotes(russianBoardUpperCase);
-          sessionStorage.setItem('lang-mode', 'rus-upper');
+          localStorage.setItem('lang-mode', 'rus-upper');
           break;
         case 'rus-upper':
           this.changeButtonsNotes(russianBoardLowerCase);
-          sessionStorage.setItem('lang-mode', 'rus-lower');
+          localStorage.setItem('lang-mode', 'rus-lower');
           break;
         case 'eng-lower':
           this.changeButtonsNotes(englishBoardUpperCase);
-          sessionStorage.setItem('lang-mode', 'eng-upper');
+          localStorage.setItem('lang-mode', 'eng-upper');
           break;
         case 'eng-upper':
           this.changeButtonsNotes(englishBoardLowerCase);
-          sessionStorage.setItem('lang-mode', 'eng-lower');
+          localStorage.setItem('lang-mode', 'eng-lower');
           break;
       
         default:
@@ -639,12 +804,111 @@ class Board {
     }
   }
 
+  applyShift() {
+    if(event.code === 'ShiftLeft' || event.code === 'ShiftRight' ) {
+      switch (localStorage.getItem('lang-mode')) {
+      
+        case 'rus-lower':
+          this.changeButtonsNotes(russianBoardShiftUpperCase);
+          break;
+        case 'rus-upper':
+          this.changeButtonsNotes(russianBoardShiftLowerCase);
+          break;
+        case 'eng-lower':
+          this.changeButtonsNotes(englishBoardShiftUpperCase);
+          break;
+        case 'eng-upper':
+          this.changeButtonsNotes(englishBoardShiftLowerCase);
+          break;
+      
+        default:
+          break;
+      }
+      
+    }
+  }
+
+  ammendShift() {
+    if(event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+
+      switch (localStorage.getItem('lang-mode')) {
+        case 'rus-lower':
+          this.changeButtonsNotes(russianBoardLowerCase);
+          localStorage.setItem('lang-mode', 'rus-lower');
+          break;
+        case 'rus-upper':
+          this.changeButtonsNotes(russianBoardUpperCase);
+          localStorage.setItem('lang-mode', 'rus-upper');
+          break;
+        case 'eng-lower':
+          this.changeButtonsNotes(englishBoardLowerCase);
+          localStorage.setItem('lang-mode', 'eng-lower');
+          break;
+        case 'eng-upper':
+          this.changeButtonsNotes(englishBoardUpperCase);
+          localStorage.setItem('lang-mode', 'eng-upper');
+          break;
+      
+        default:
+          break;
+      }
+      
+    }
+  }
+
+  applyShiftOnClick() {
+    if (event.target.innerHTML === 'Shift') {
+      switch (localStorage.getItem('lang-mode')) {
+      
+        case 'rus-lower':
+          this.changeButtonsNotes(russianBoardShiftUpperCase);
+          break;
+        case 'rus-upper':
+          this.changeButtonsNotes(russianBoardShiftLowerCase);
+          break;
+        case 'eng-lower':
+          this.changeButtonsNotes(englishBoardShiftUpperCase);
+          break;
+        case 'eng-upper':
+          this.changeButtonsNotes(englishBoardShiftLowerCase);
+          break;
+      
+        default:
+          break;
+      }
+
+      setTimeout( () => {
+        switch (localStorage.getItem('lang-mode')) {
+          case 'rus-lower':
+            this.changeButtonsNotes(russianBoardLowerCase);
+            localStorage.setItem('lang-mode', 'rus-lower');
+            break;
+          case 'rus-upper':
+            this.changeButtonsNotes(russianBoardUpperCase);
+            localStorage.setItem('lang-mode', 'rus-upper');
+            break;
+          case 'eng-lower':
+            this.changeButtonsNotes(englishBoardLowerCase);
+            localStorage.setItem('lang-mode', 'eng-lower');
+            break;
+          case 'eng-upper':
+            this.changeButtonsNotes(englishBoardUpperCase);
+            localStorage.setItem('lang-mode', 'eng-upper');
+            break;
+        
+          default:
+            break;
+        }
+      }, 1000);
+    }
+  
+  }    
 }
 
 const board = new Board();
 board.textArea = board.createTextArea();
-if (!sessionStorage.getItem('lang-mode')) {sessionStorage.setItem('lang-mode', 'eng-lower')};
-let stor = sessionStorage.getItem('lang-mode');
+if (!localStorage.getItem('lang-mode')) {localStorage.setItem('lang-mode', 'eng-lower')};
+let stor = localStorage.getItem('lang-mode');
 let langMode;
 switch (stor) {
   case 'rus-lower':
